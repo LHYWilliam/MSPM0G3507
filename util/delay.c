1cb1d3be8a4dd8c249d6536078b5c68d42444a38
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2024-07-23 13:55:18 +0800

    Add stop to Systick

--- util/delay.c
+++ util/delay.c
@@ -3,8 +3,12 @@
 volatile uint32_t globalms = 0;
 
 void Delay_ms(uint32_t ms) {
+	SYSCFG_DL_SYSTICK_init();
+	
 	globalms = ms;
 	while(globalms);
+	
+	SysTick->CTRL = 0;
 }
 
 void SysTick_Handler(void) {
